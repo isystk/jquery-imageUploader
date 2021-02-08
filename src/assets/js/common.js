2d9cfb6e0b7478a39600f090baf30b838237ab0d
iseyoshitaka <iseyoshitaka@team-lab.com>
2021-02-08 09:27:34 +0900

    画像の回転作成中

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -76,7 +76,7 @@ $(function() {
 				'<tr>',
 					'<td>'+res.fileName+'</td>',
 					'<td><img src="'+res.ofileData+'" /></td>',
-					'<td><img src="'+res.fileData+'" /></td>',
+					'<td><img class="js-view-canvas" src="'+res.fileData+'" /></td>',
 					'<td>'+res.ofileSize+'</td>',
 					'<td>'+res.fileSize+'</td>',
 					'<td>'+res.fileType+'</td>',
@@ -88,7 +88,49 @@ $(function() {
 			$('.error-message').text(res[0]);
 		}
 	});
-		
+
+    $(document).on('click', '.js-view-canvas', function() {
+        //2Dコンテキストのオブジェクトを生成する
+        var canvasEdit = $('#canvas-edit');
+        var ctx= canvasEdit[0].getContext('2d');
+        //画像オブジェクトを生成
+        var image = new Image();
+        image.src = $(this).attr('src');
+        var thumbnail_width = '300';
+        var thumbnail_height = '300';
+        //画像をcanvasに設定
+        image.onload = function(){
+    		var width, height;
+			if(image.width > image.height){
+				// 横長の画像は横のサイズを指定値にあわせる
+				var ratio = image.height/image.width;
+				width = thumbnail_width;
+				height = thumbnail_width * ratio;
+			} else {
+				// 縦長の画像は縦のサイズを指定値にあわせる
+				var ratio = image.width/image.height;
+				width = thumbnail_height * ratio;
+				height = thumbnail_height;
+			}
+            canvasEdit.attr('width', width)
+                .attr('height', height);
+            ctx.drawImage(image, 0, 0, width, height);
+        }
+        canvasEdit.click(function() {
+            var width = 300;
+            var height = 300;
+            // コンテキストを保存する
+            ctx.clearRect(0, 0, canvasEdit.width, canvasEdit.height);
+            // 回転の中心に原点を移動する
+            ctx.translate(width, height);
+            // canvasを回転する
+            ctx.rotate(90 * Math.PI/180);
+            // 画像サイズの半分だけずらして画像を描画する
+            ctx.drawImage(image, -(width/2), -(height/2));
+            // コンテキストを元に戻す
+        });
+    });
+
 });
 
 
